121a35ac93cefe84ff516750f29c4f392a2709a0
kuwar jha <kuwarjha.cse@gmail.com>
2025-05-02 19:01:20 +0530

    added asynchandler, apierror and apiresonse format

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
-const asyncHandler = () =>{}
+const asyncHandler = (func) => {
+  (req, res, next) => {
+    Promise.resolve(func(req, res, next)).catch((err) => next(err));
+  };
+};
 
-
-
-export {asyncHandler}
+export { asyncHandler };
