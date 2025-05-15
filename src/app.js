afbfd573f2a8adc6f398bdec54885f0856229cf0
kuwar jha <kuwarjha.cse@gmail.com>
2025-05-15 15:23:34 +0530

    added global error handler

--- src/app.js
+++ src/app.js
@@ -20,16 +20,35 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
-
-
 //Routes import
 
 import userRouter from './routes/user.rt.js'
+import { ApiError } from "./utils/apiError.js";
+
 
 
 app.use("/api/v1/user", userRouter)
 
+
+
+
+
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      success: err.success,
+      message: err.message,
+      errors: err.errors,
+    });
+  }
+
+  console.error("Unhandled Error:", err);
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal server error",
+  });
+});
 
 export default app;
 
